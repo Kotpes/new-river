9315eaa2f49f74bbd2db9f1d97a86bcb71fffc47
Sigy <kotpes@gmail.com>
2013-03-07 17:53:27 +0400

    working form, deleted unecessary files, opera and mozilla fix

---
+++ contact-form-handler.php
@@ -0,0 +1,46 @@
+<?php 
+$errors = '';
+$myemail = 'kotpes@gmail.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  || 
+   empty($_POST['number']))
+{
+    $errors .= "\n Пожалуйста, заполните все поля";
+}
+
+$name = $_POST['name']; 
+$number = $_POST['number']; 
+$date = $_POST['date']; 
+$ship = $_POST['ship']; 
+$message = $_POST['message']; 
+
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "Contact form submission: $name";
+	$email_body = "You have received a new message. ".
+	" Here are the details:\n Name: $name \n Number: $number \n Date: \n $date
+	\n Ship: \n $ship \n Message: \n $message"; 
+	
+	$headers = "From: $myemail\n"; 
+	
+	
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: contact-form-thank-you.html');
+} 
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+	<title>Contact form handler</title>
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
+
+
+</body>
+</html>
--- js/script.js
+++ js/script.js
@@ -6,6 +6,12 @@ $(document).ready(function() {
     $('#slideshow_banner').hide().delay(800).fadeTo(1000, 1).end();
     $('#add').hide().delay(3200).fadeTo(1000, 1).end();
     $('#maksimus #picture').hide().delay(2800).fadeTo(1000, 1).end();
+
+    $('#phone').hide().delay(2800).fadeTo(1000, 1).end();
+    $('#heading').hide().delay(2800).fadeTo(1000, 1).end();
+    $('#heading_big').hide().delay(2800).fadeTo(1000, 1).end();
+    
+
     $('#maksimus').hide().delay(2600).fadeTo(1000, 1).end();
     $('#addservices').hide().delay(3000).fadeTo(1000, 1).end();
     $('#lidia #picture').hide().delay(2800).fadeTo(1000, 1).end();
@@ -24,9 +30,16 @@ $(document).ready(function() {
        $('#addinfo2').fadeTo(150, 0).css('z-index', '15'); 
        });   
     $('#add').click(function() {
-      $('#addservices').slideToggle('300');
-      });
-    });
+      $('#addservices').slideToggle('200');
+      });  
+    $('#form').css('display', 'none');
+    $('a, .close').click(function() {
+      $('#form').slideToggle('200');
+      }); 
+   });
+
+
+
 
 /** Slideshow **/
 
